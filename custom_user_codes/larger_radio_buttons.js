471b0d40b2eaf26c8632f21b6fa5817404dbc912
stephluma <166639835+stephluma@users.noreply.github.com>
2026-02-21 15:59:43 -0800

    Create larger_radio_buttons.js

---
+++ custom_user_codes/larger_radio_buttons.js
@@ -0,0 +1,2 @@
+//change radio buttons to menuButtonLarge //
+<style>.menuButtonLarge {  float: left;  width: 320px;  height: 80px;  cursor: pointer;  margin: 12px;  padding: 2px;  box-shadow: 0 2px 6px #8e8e8e;  transition: box-shadow 0.3s ease;}</style>
