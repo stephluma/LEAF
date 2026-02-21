2015c28355de6c556bcb97324ea0a210495bdd57
stephluma <166639835+stephluma@users.noreply.github.com>
2026-02-21 15:56:30 -0800

    Create custom_workflow_icons.js

---
+++ custom_user_codes/custom_workflow_icons.js
@@ -0,0 +1,7 @@
+<script>
+async function main_customActionButton() {
+   document.querySelector('#button_step1_approve').innerHTML = '<img src="dynicons/?img=gnome-emblem-default.svg&amp;w=22" alt="" style="vertical-align: middle" /> Approve';
+}
+
+document.addEventListener('DOMContentLoaded', main_customActionButton);
+</script> 
