5219907e8435b4f4ed5c0086cc454ffb2400df2a
stephluma <166639835+stephluma@users.noreply.github.com>
2026-02-21 16:06:22 -0800

    Create disable_field.js

---
+++ custom_user_codes/form_fields/disable_field.js
@@ -0,0 +1,7 @@
+<script>
+  // Disable the input with the dynamic ID and apply gray background color
+  $('#{{ iID }}').attr('disabled', true).css('background-color', '#e0e0e0');
+
+  // Disable the input with INDICATOR_ID and apply gray background color
+  $('#INDICATOR_ID').attr('disabled', true).css('background-color', '#e0e0e0');
+</script>
